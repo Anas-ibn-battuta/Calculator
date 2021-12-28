d47146bb222d07fa56c42a334a0c2af320b331f0
Anas-Tawalbeh <59394028+Anas-Tawalbeh@users.noreply.github.com>
2021-12-28 09:07:21 +0700

    Add files via upload
    This is the starting file ...

---
+++ calc.c
@@ -0,0 +1,23 @@
+// preprocessor
+#include <stdio.h>
+
+/*the main function */
+int main() {
+   //variable declaration
+   int number1=9, number2=3, sum, subtraction, multi;
+   double div;
+   printf("First Number is: %d, and the second Number is: %d\n", number1, number2);
+   //addition statement
+   sum=number1+number2;
+   printf("The total is: %d\n", sum);
+   //subtraction statement
+   subtraction=number1-number2;
+   printf("The subtraction of the two values is: %d\n", subtraction)
+   //multiplication statement
+   multi=number1*number2;
+   printf("The multiplication result is: %d\n", multi);
+   //division statement
+   div=number1/number2;
+   printf("The division result is: %f\n", div);
+   /*C language statements */
+}
